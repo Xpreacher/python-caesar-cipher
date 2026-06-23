d46e7cec9c6d9b6235a1827323afbdca9ebb984b
Xpreacher <onlyxpreacher@gmail.com>
2026-06-23 11:21:19 +0100

    Added Caesar Cipher project

---
+++ art.py
@@ -0,0 +1,17 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"    ""     `Y8 a8P_____88 I8[    ""     `Y8 88P'   "Y8  
+8b       ,adPPPPP88 8PP"""""""     `Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88          
+            88                                 88             
+           ""                                 88             
+ ,adPPYba, 88 8b,dPPYba,  ,adPPYba, ,adPPYba, 8b,dPPYba,  
+a8"    "" 88 88P'    "8a 8a,   ,aa 8a,   ,aa 88P'   "Y8  
+8b       88 88       d8 `Ybbd8"' `Ybbd8"' 88          
+"8a,   ,aa 88 88b,   ,a8 ,8a,   ,aa ,8a,   ,aa 88          
+ `"Ybbd8"' 88 `"YbbdP"'  `"Ybbd8"' `"YbbdP"' 88          
+            88                                 88             
+           ""                                 88             
+"""
+
